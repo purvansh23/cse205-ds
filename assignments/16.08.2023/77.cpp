7a393e50531c2536532de6eb0363b79b7691bb88
Purvansh Desai <purvansh.desai07@gmail.com>
2023-08-19 20:05:41 +0530

    assignments added

---
+++ assignments/16.08.2023/77.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+void solve(vector<int> input,vector<int> output,int index,vector<vector<int>>& ans){
+     if(index>=input.size()){
+         ans.push_back(output);
+         return ;
+     }
+     //exclude
+     solve(input,output,index+1,ans);
+
+    //include
+    int element=input[index];
+    output.push_back(element);
+    solve(input,output,index+1,ans);
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> > ans;
+        vector<int> output;
+        int index = 0;
+        solve(nums, output, index, ans);
+        return ans;
+    }
+
