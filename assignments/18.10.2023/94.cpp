d7a625c7d16ad9999d472b16183ca402f5fd049d
Purvansh Desai <purvansh.desai07@gmail.com>
2023-10-18 14:24:17 +0530

    added

---
+++ assignments/18.10.2023/94.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorder(root, result);
+        return result;
+    }
+    
+    void inorder(TreeNode* node, vector<int>& result) {
+        if (node == nullptr) {
+            return;
+        }
+        
+        // Traverse the left subtree
+        inorder(node->left, result);
+        
+        // Visit the current node
+        result.push_back(node->val);
+        
+        // Traverse the right subtree
+        inorder(node->right, result);
+    }
+};
